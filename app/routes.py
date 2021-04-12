b618df04465c2066889e79ee32dd4c5ed2af5821
Angga NIX <angga@sard.maxindo.net.id>
2021-04-12 22:29:35 +0700

    Menambahkan routes kategori dari controller Kategori Class

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import jsonify
 from app import app
+from app.controllers import Kategori
+
+# Class Instance
+kategori = Kategori.Kategori()
 
 
 @app.route("/")
 def index():
-    return "Hello World!ssss 123 123"
+    return jsonify({
+        "result": "home rest api"
+    })
+
+
+@app.route("/kategori")
+def kategori_index():
+    return kategori.kategori_index()
