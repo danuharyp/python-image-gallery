a96f7ebb8f9f90fe848d7656237c87c3df45ae27
Angga NIX <angga@sard.maxindo.net.id>
2021-04-13 14:55:50 +0700

    Menambahkan image gallery route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import app
-from app.controllers import Kategori, Akun
+from app.controllers import Kategori, Akun, ImageGallery
 from flask_cors import CORS
 
 CORS(app)
@@ -8,6 +8,7 @@ CORS(app)
 # Class Instance
 kategori = Kategori.Kategori()
 akun = Akun.Akun()
+image_gallery = ImageGallery.ImageGallery()
 
 
 @app.route("/", methods=['GET'])
@@ -86,9 +87,55 @@ def delete_akun(id):
         akun.id = id
         return akun.delete_akun()
 
+
 @app.route("/login", methods=['POST'])
 def login_akun():
     if request.method == 'POST':
         akun.username = request.form['username']
         akun.password = request.form['password']
         return akun.login_akun()
+
+# Image Gallery Route List
+
+
+@app.route("/image_gallery", methods=['GET'])
+def all_image_gallery():
+    return image_gallery.get_all_image_gallery()
+
+
+@app.route("/image_gallery/<int:id>", methods=['GET'])
+def get_image_gallery(id):
+    return image_gallery.get_image_gallery(id)
+
+
+@app.route("/image_gallery/insert", methods=['POST'])
+def insert_image_gallery():
+    if request.method == 'POST':
+        upload_file = image_gallery.upload_images(request.files)
+
+        if upload_file:
+            image_gallery.id_kategori = request.form['id_kategori']
+            image_gallery.id_akun = request.form['id_akun']
+            image_gallery.judul = request.form['judul']
+            image_gallery.caption = request.form['caption']
+            return image_gallery.insert_images()
+        else:
+            return jsonify({
+                "status": False
+            })
+
+
+# @app.route("/image_gallery/update/<int:id>", methods=['PUT'])
+# def update_image_gallery(id):
+#     if request.method == 'PUT':
+#         image_gallery.id = id
+#         image_gallery.nama_lengkap = request.form['nama_lengkap']
+#         image_gallery.email = request.form['email']
+#         return image_gallery.update_image_gallery()
+
+
+# @app.route("/image_gallery/delete/<int:id>", methods=['DELETE'])
+# def delete_image_gallery(id):
+#     if request.method == 'DELETE':
+#         akun.id = id
+#         return akun.delete_akun()
